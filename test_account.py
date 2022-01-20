49d4a2b8dd3352bf63d15595351ae6c73f068efe
Your Name  git config --global user.email you@example.comgit config --global user.email holt.codes@gmail.com <you@example.com>
2022-01-19 17:37:35 -0800

    commit

---
+++ test_account.py
@@ -0,0 +1,16 @@
+class BankAccount:
+  bank_name = "Bank Holt"
+  all_accounts = []
+
+def __init__(self, int_rate, balance):
+  self.int_rate = int_rate,balance):
+  self.balance = balance
+  BankAccount.all_accounts.append(self)
+
+def deposit(self, amount):
+
+def withdraw(self, amount):
+
+def display_account_info(self):
+
+def yield_interest(self):
