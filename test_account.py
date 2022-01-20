c760f961cffb158a4ea4e4de5f9c61d403ee9ab3
Your Name  git config --global user.email you@example.comgit config --global user.email holt.codes@gmail.com <you@example.com>
2022-01-19 19:29:21 -0800

    commit

--- test_account.py
+++ test_account.py
@@ -1,16 +1,30 @@
-class BankAccount:
-  bank_name = "Bank Holt"
-  all_accounts = []
+class BankAccount():
+  accounts = []
+  def __init__(self, int_rate, balance): 
+    self.int_rate = int_rate
+    self.balance = balance
 
-def __init__(self, int_rate, balance):
-  self.int_rate = int_rate,balance):
-  self.balance = balance
-  BankAccount.all_accounts.append(self)
+  def deposit(self, amount):
+    self.deposit += amount
+    return self
 
-def deposit(self, amount):
+  def withdraw(self, amount):
+    if(self.balance - amount) >= 0:
+      self.balance -= amount
+    else:
+      print("Insuffcient Funds: Charging a $5 fee")  
+      self.balance -= 5
+      return self
 
-def withdraw(self, amount):
+  def display_account_info(self):
+    print(f"Balance: {self.balance}")
+    return self
 
-def display_account_info(self):
+  def yield_interest(self):
+    if self.balance > 0:
+      self.balance += (self.balance * self.int_rate)
+      return self
 
-def yield_interest(self):
+
+checking = BankAccount (.03, 500)
+savings = BankAccount (.05, 1000)
